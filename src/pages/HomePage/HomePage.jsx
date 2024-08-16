b2b95dd86ab7471d41e86a1ead0cbde8301012af
MaksymSobotiuk <maxim.4.july.2001@gmail.com>
2024-08-16 09:12:46 +0300

    done

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import s from './HomePage.module.css'
+
+const HomePage = () => {
+    return (
+        <div className={s.wrapper}>
+            <h1>Тут може бути ваша реклама</h1>
+        </div>
+    )
+};
+
+export default HomePage;
